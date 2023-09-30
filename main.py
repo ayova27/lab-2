0a1bfd52c28ceda14662a93f385fd63e6529a103
ayova27 <dausaduchex@gmail.com>
2023-09-30 14:54:10 +0600

    main

--- main.py
+++ main.py
@@ -27,4 +27,31 @@ def roskomnadzor():
 
 # roskomnadzor()
 
-def 
+def arithmetic_progression():
+    try:
+        user_input_number = int(input("First number: "))
+        user_input_number2 = int(input("Second number: "))
+        user_input_number3 = int(input("Third number: "))
+
+        if (user_input_number + user_input_number2) == user_input_number3:
+            print("Yes!")
+        else:
+            print("No!")
+    except ValueError:
+        print("Please, enter number!")
+
+
+def rook_move():
+    chess_board = 8
+
+    for i in range(chess_board):
+        for j in range(chess_board):
+            if i == 5 and j == 4:
+                continue
+            if (i + j) % 2 == 0:
+                print("■", end=" ")
+            else:
+                print("□", end=" ")
+        print()
+
+rook_move()
